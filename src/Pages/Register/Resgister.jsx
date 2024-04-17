261399352b8f1c883cfbff657519ee046fdfeeb7
DevSadit <de.sadit@gmail.com>
2024-04-17 19:42:42 +0600

    pas verificatuion in register page completed!

--- src/Pages/Register/Resgister.jsx
+++ src/Pages/Register/Resgister.jsx
@@ -2,23 +2,40 @@ import { Link } from "react-router-dom";
 import Head from "../../Layout/Head";
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Resgister = () => {
   const { creatUser } = useContext(AuthContext);
   const handleRegister = (e) => {
     e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-            
-    // 
 
-    creatUser(email, password)
-      .then((result) => result)
-      .catch((error) => console.log(error));
+        email.value = "";
+        password.value = "";
+
+
+    const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z]).{6,}$/;
+    const password = e.target.password.value;
+    const email = e.target.email.value;
+    if (!passwordPattern.test(password)) {
+      toast.warn(`Write The Password Correctly`);
+      console.log(
+        `Password must have at least one uppercase letter, one lowercase letter, and be at least 6 characters long`
+      );
+    } else {
+      toast.success(`Registered succesfully!`);
+      console.log("Password is valid.");
+      creatUser(email, password)
+        .then((result) => result)
+        .catch((error) => console.log(error));
+    }
+
+
+    //
   };
   return (
     <div>
-      <Head title="Register" />;
+      <Head title="Register" />;<ToastContainer></ToastContainer>
       <div className="hero min-h-screen ">
         <div className="hero-content flex-col lg:flex-row w-full">
           <div className="text-center lg:text-left">
